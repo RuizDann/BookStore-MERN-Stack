0498b6eacd223361488022f8be312b1617073673
Daniel Ruiz <108552382+RuizDann@users.noreply.github.com>
2023-11-02 16:34:35 -0700

    updated dependencies

--- Backend/config.js
+++ Backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5500;
 
 export const MONGO_URL =
-    "mongodb+srv://bookstoreuser:bookstoreuser@book-store-mern.op6tzbt.mongodb.net/books-collection?retryWrites=true&w=majority";
+    "mongodb+srv://bookstoreuser:bookstoreuser@book-store-mern.op6tzbt.mongodb.net/?retryWrites=true&w=majority";
